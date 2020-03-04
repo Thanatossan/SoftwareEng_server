69dd5ccba5bb0c0d771b19679d4a71bb648d2356
SuphakornHomnan <supakornhomnan@gmail.com>
2020-03-04 22:01:13 +0700

    nice

--- controllers/todolist_controller.js
+++ controllers/todolist_controller.js
@@ -1,18 +1,56 @@
 const database = require('../database')
 
 module.exports = {
-    add_topic(req,res,next) {
+
+    add_title(req, res, next) {
         const data = req.body
-        //print object เพื่อดูว่าจะเอาข้อมูลมาใช้อะไร
         console.log(data)
-        database.query(
-            "INSERT INTO `todolist`(`Todolist_id`,`title`,`description`,`priority_level`,`deadline`) VALUES (value1, value2, value3, ...)")
+        // print object เพื่อดูว่าจะเอาข้อมูลมาใช้อะไร
+        const title = req.body.title
+        const isComplete = req.body.iscom
+        const student_id = req.body.id
+        const sql = "INSERT INTO `todolist` ( `title`,`isComplete`,`student_code`) VALUES('" + title + "','" + isComplete + "'," + student_id + ")"
+        //console.log(sql);
+        console.log(data)
+        database.query(sql)
+        res.end()
     },
-    show_todolist(req,res,next){
-        database.query("select * from `todolist`",
-        (err,data)=>{
-            console.log(err)
+
+    show_title(req, res, next) {
+        
+        const sql = 'select title,isComplete from todolist'
+        console.log(sql);
+        
+        database.query(sql, (err, data) => {
+            console.log(data);
             res.json(data)
         })
+    },
+
+    update_data(req, res, next) {
+        // const data = req.body
+        // console.log(data)
+        const description = req.body.description
+        const priority = parseInt(req.body.priority)
+        const deadline = req.body.deadline.toString()
+        const id = parseInt(req.body.id)
+
+        const sql_1 = "SELECT MAX(Todolist_id) as id FROM `todolist` WHERE `student_code` = " + id + " ORDER BY `isComplete`"
+
+
+        database.query(sql_1, (err, data) => {
+            const id_max = data[0].id
+            console.log(id_max);
+
+            const sql_2 = "UPDATE `todolist` SET `description` = '" + description + "', `priority_level` = " + priority + ", `deadline` = '" + deadline + "' WHERE `Todolist_id` = " + id_max
+            console.log(sql_2)
+            database.query(sql_2)
+            res.send("update!!!!!")
+            res.end()
+        })
+
+        // const sql = "UPDATE `todolist` `descrition` = 'descrition', `priority_level` = 'priority', `deadline` = 'deadline',`student_id` = 'id' WHERE "
+
     }
+
 }
