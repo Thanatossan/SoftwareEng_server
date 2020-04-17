ff3aaecdafe72ee8dc59c08874b2f42aa8935b3b
SuphakornHomnan <supakornhomnan@gmail.com>
2020-04-17 17:23:52 +0000

    add getuserdetail again

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -58,7 +58,7 @@ module.exports = {
     },
     getDetail(req, res, next) {
         const studentID = req.params.id
-        const selectTitle = 'SELECT * FROM todolist WHERE student_id = ' + studentID
+        const selectTitle = 'SELECT * FROM users WHERE student_id = ' + studentID
         // console.log(selectTitle);
         database.query(selectTitle, (err, data) => {
             // console.log(data);
